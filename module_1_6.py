fe19d09886c5bf2e2637f69f10d1e8fcf70132e2
Дмитрий Янно <ugor.finno@yandex.ru>
2024-12-04 16:53:45 +0500

    create Practice for Variables

---
+++ module_1_6.py
@@ -0,0 +1,5 @@
+homework_completed = 12
+spent_hours = 1.5
+course_name = 'Python'
+average_time = spent_hours / homework_completed
+print(f"Курс: '{course_name}', затрачено часов: {spent_hours}, среднее время выполнения: {average_time} часа ")
