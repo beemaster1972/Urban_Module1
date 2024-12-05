6796cfefb5502b6eec7ab2980129b803c49b9bf8
Дмитрий Янно <ugor.finno@yandex.ru>
2024-12-05 14:19:18 +0500

    changes in task 3 and 9

---
+++ module_1_3.py
@@ -0,0 +1,8 @@
+name = 'Dmitry'
+print('My name is', name)
+age = 52
+print('My age is',age)
+age = age + 1
+print('My new age is',age)
+is_student = True
+print('Is me a student? It`s', is_student)
